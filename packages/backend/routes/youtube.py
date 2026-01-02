6500d46511828d9b8186bf11e011abac5e4cc6d0
TakekuniT <takekuni@tanemori.org>
2026-01-02 00:33:37 -0500

    keys and scope

--- packages/backend/routes/youtube.py
+++ packages/backend/routes/youtube.py
@@ -1,6 +1,16 @@
 from fastapi import APIRouter
+from fastapi.responses import RedirectResponse
+import os
+import urllib.parse
+import requests
+
 
 router = APIRouter()
+
+CLIENT_ID = os.environ.get("YOUTUBE_CLIENT_ID")
+CLIENT_SECRET = os.environ.get("YOUTUBE_CLIENT_SECRET")
+REDIRECT_URI = os.environ.get("YOUTUBE_REDIRECT_URI")
+SCOPES = ["https://www.googleapis.com/auth/youtube.upload"]
 
 @router.get("/")
 def test_youtube():
