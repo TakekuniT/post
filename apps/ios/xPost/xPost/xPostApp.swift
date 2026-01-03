fd4d24dcbeeb83f2af71a7524185f2ec5ecc1346
TakekuniT <takekuni@tanemori.org>
2026-01-03 11:58:31 -0500

    complete ios setup with updated git ignore

---
+++ apps/ios/xPost/xPost/xPostApp.swift
@@ -0,0 +1,17 @@
+//
+//  xPostApp.swift
+//  xPost
+//
+//  Created by Takekuni Tanemori on 1/3/26.
+//
+
+import SwiftUI
+
+@main
+struct xPostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ apps/ios/xPost/xPostTests/xPostTests.swift
@@ -0,0 +1,16 @@
+//
+//  xPostTests.swift
+//  xPostTests
+//
+//  Created by Takekuni Tanemori on 1/3/26.
+//
+
+import Testing
+
+struct xPostTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
